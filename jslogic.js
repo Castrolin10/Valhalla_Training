aae8d0a0999e69e044bcaded0c7f64970262f023
Castrolin10 <160283709+Castrolin10@users.noreply.github.com>
2024-05-03 19:57:18 -0500

    Add files via upload

---
+++ jslogic.js
@@ -0,0 +1,560 @@
+const productos = [
+  {
+  idProducto: "F78K9mnbG654", // ID válido
+  nombreProducto: "Camiseta de fútbol",
+  selectorCategoria: "futbol",
+  imagen: "1.jpg",
+  precio: 65000,
+  color: "Azul marino",
+  garantia: "1 año",
+  material: "Poliéster transpirable"
+},
+
+// Objeto 2
+ objeto2 = {
+  idProducto: "B4ytg765K987", // ID válido
+  nombreProducto: "Zapatillas de baloncesto",
+  selectorCategoria: "baloncesto",
+  imagen: "5.jpg",
+  precio: 150000,
+  color: "Blanco y negro",
+  garantia: "6 meses",
+  material: "Cuero sintético y goma"
+},
+
+// Objeto 3
+ objeto3 = {
+  idProducto: "T98y7t65r4321", // ID válido
+  nombreProducto: "Raqueta de tenis profesional",
+  selectorCategoria: "tennis",
+  imagen: "6.jpg",
+  precio: 280000,
+  color: "Negro y amarillo",
+  garantia: "2 años",
+  material: "Grafito y carbono"
+},
+
+// Objeto 4
+ objeto4 = {
+  idProducto: "H8y7t65r4321", // ID válido
+  nombreProducto: "Balón de voleibol playa",
+  selectorCategoria: "voley",
+  imagen: "7.jpg",
+  precio: 35000,
+  color: "Naranja y azul",
+  garantia: "1 año",
+  material: "Cuero sintético y goma"
+},
+
+// Objeto 5
+ objeto5 = {
+  idProducto: "J78K9mnbG654", // ID válido
+  nombreProducto: "Mancuernas de gimnasio",
+  selectorCategoria: "gym",
+  imagen: "8.jpg",
+  precio: 80000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Hierro fundido"
+},
+
+// Objeto 6
+ objeto6 = {
+  idProducto: "A78K9mnbG654", // ID válido
+  nombreProducto: "Bicicleta de montaña para niños",
+  selectorCategoria: "ciclismo",
+  imagen: "9.jpg",
+  precio: 135000,
+  color: "Azul y verde",
+  garantia: "2 años",
+  material: "Aluminio"
+},
+
+// Objeto 7
+ objeto7 = {
+  idProducto: "S78K9mnbG654", // ID válido
+  nombreProducto: "Patines en línea para adultos",
+  selectorCategoria: "patinaje",
+  imagen: "1.jpg",
+  precio: 110000,
+  color: "Negro y rojo",
+  garantia: "1 año",
+  material: "Polipropileno y aluminio"
+},
+
+// Objeto 8
+ objeto8 = {
+  idProducto: "D78K9mnbG654", // ID válido
+  nombreProducto: "Casco de bicicleta con luces",
+  selectorCategoria: "ciclismo",
+  imagen: "2.jpg",
+  precio: 75000,
+  color: "Blanco y negro",
+  garantia: "2 años",
+  material: "ABS y policarbonato"
+},
+
+// Objeto 9
+ objeto9 = {
+  idProducto: "F78K9mnbG654", // ID válido
+  nombreProducto: "Raqueta de bádminton",
+  selectorCategoria: "badminton",
+  imagen: "2.jpg",
+  precio: 50000,
+  color: "Naranja y amarillo",
+  garantia: "1 año",
+  material: "Grafito y aluminio"
+},// Objeto 10
+{
+  idProducto: "G78K9mnbG654",
+  nombreProducto: "Red de fútbol",
+  selectorCategoria: "futbol",
+  imagen: "10.jpg",
+  precio: 45000,
+  color: "Blanco",
+  garantia: "1 año",
+  material: "Nylon resistente"
+},
+// Objeto 11
+{
+  idProducto: "K78K9mnbG654",
+  nombreProducto: "Canasta de baloncesto portátil",
+  selectorCategoria: "baloncesto",
+  imagen: "11.jpg",
+  precio: 120000,
+  color: "Rojo y negro",
+  garantia: "2 años",
+  material: "Acero galvanizado"
+},
+// Objeto 12
+{
+  idProducto: "L78K9mnbG654",
+  nombreProducto: "Pelotas de tenis",
+  selectorCategoria: "tennis",
+  imagen: "12.jpg",
+  precio: 5000,
+  color: "Amarillo",
+  garantia: "6 meses",
+  material: "Goma vulcanizada"
+},
+// Objeto 13
+{
+  idProducto: "M78K9mnbG654",
+  nombreProducto: "Red de voleibol indoor",
+  selectorCategoria: "voley",
+  imagen: "13.jpg",
+  precio: 60000,
+  color: "Blanco",
+  garantia: "1 año",
+  material: "Polietileno resistente"
+},
+// Objeto 14
+{
+  idProducto: "N78K9mnbG654",
+  nombreProducto: "Pesas para tobillos",
+  selectorCategoria: "gym",
+  imagen: "14.jpg",
+  precio: 35000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Neopreno y hierro"
+},
+// Objeto 15
+{
+  idProducto: "O78K9mnbG654",
+  nombreProducto: "Cascos para patinaje",
+  selectorCategoria: "patinaje",
+  imagen: "15.jpg",
+  precio: 85000,
+  color: "Azul y blanco",
+  garantia: "2 años",
+  material: "Policarbonato y espuma EPS"
+},
+// Objeto 16
+{
+  idProducto: "P78K9mnbG654",
+  nombreProducto: "Guantes para ciclismo",
+  selectorCategoria: "ciclismo",
+  imagen: "16.jpg",
+  precio: 25000,
+  color: "Negro y rojo",
+  garantia: "1 año",
+  material: "Spandex y microfibra"
+},
+// Objeto 17
+{
+  idProducto: "Q78K9mnbG654",
+  nombreProducto: "Volantes de bádminton",
+  selectorCategoria: "badminton",
+  imagen: "17.jpg",
+  precio: 2000,
+  color: "Blanco",
+  garantia: "6 meses",
+  material: "Plástico y corcho"
+},
+// Objeto 18
+{
+  idProducto: "R78K9mnbG654",
+  nombreProducto: "Tablas de surf",
+  selectorCategoria: "surf",
+  imagen: "18.jpg",
+  precio: 300000,
+  color: "Varios",
+  garantia: "2 años",
+  material: "Espuma de poliuretano y fibra de vidrio"
+},
+// Objeto 19
+{
+  idProducto: "S78K9mnbG654",
+  nombreProducto: "Bolsa de deporte",
+  selectorCategoria: "accesorios",
+  imagen: "19.jpg",
+  precio: 40000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Poliéster resistente"
+},
+{
+  idProducto: "T78K9mnbG654",
+  nombreProducto: "Gorra de tenis",
+  selectorCategoria: "tennis",
+  imagen: "20.jpg",
+  precio: 25000,
+  color: "Blanco",
+  garantia: "6 meses",
+  material: "Algodón y poliéster"
+},
+// Objeto 21
+{
+  idProducto: "U78K9mnbG654",
+  nombreProducto: "Pelota de rugby",
+  selectorCategoria: "rugby",
+  imagen: "21.jpg",
+  precio: 30000,
+  color: "Rojo y negro",
+  garantia: "1 año",
+  material: "Caucho vulcanizado"
+},
+// Objeto 22
+{
+  idProducto: "V78K9mnbG654",
+  nombreProducto: "Soga de entrenamiento",
+  selectorCategoria: "gym",
+  imagen: "22.jpg",
+  precio: 15000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Nylon resistente"
+},
+// Objeto 23
+{
+  idProducto: "W78K9mnbG654",
+  nombreProducto: "Guantes de boxeo",
+  selectorCategoria: "boxeo",
+  imagen: "23.jpg",
+  precio: 50000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Cuero sintético y espuma"
+},
+// Objeto 24
+{
+  idProducto: "X78K9mnbG654",
+  nombreProducto: "Máscara de snorkel",
+  selectorCategoria: "natacion",
+  imagen: "24.jpg",
+  precio: 40000,
+  color: "Azul y transparente",
+  garantia: "2 años",
+  material: "Silicona y cristal templado"
+},
+// Objeto 25
+{
+  idProducto: "Y78K9mnbG654",
+  nombreProducto: "Red de bádminton",
+  selectorCategoria: "badminton",
+  imagen: "25.jpg",
+  precio: 20000,
+  color: "Blanco",
+  garantia: "1 año",
+  material: "Nylon resistente"
+},
+// Objeto 26
+{
+  idProducto: "Z78K9mnbG654",
+  nombreProducto: "Balón medicinal",
+  selectorCategoria: "gym",
+  imagen: "26.jpg",
+  precio: 40000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Piel sintética y relleno de arena"
+},
+// Objeto 27
+{
+  idProducto: "A78K9mnbG654",
+  nombreProducto: "Máscara de buceo",
+  selectorCategoria: "buceo",
+  imagen: "27.jpg",
+  precio: 70000,
+  color: "Negro y amarillo",
+  garantia: "2 años",
+  material: "Silicona y policarbonato"
+},
+// Objeto 28
+{
+  idProducto: "B78K9mnbG654",
+  nombreProducto: "Cuerda para saltar",
+  selectorCategoria: "gym",
+  imagen: "28.jpg",
+  precio: 10000,
+  color: "Negro y verde",
+  garantia: "1 año",
+  material: "Cuerda de nylon y mangos de espuma"
+},
+// Objeto 29
+{
+  idProducto: "C78K9mnbG654",
+  nombreProducto: "Set de tenis de mesa",
+  selectorCategoria: "pingpong",
+  imagen: "29.jpg",
+  precio: 80000,
+  color: "Azul y blanco",
+  garantia: "1 año",
+  material: "Madera y goma"
+},
+{
+  idProducto: "D78K9mnbG654",
+  nombreProducto: "Bolsa de boxeo",
+  selectorCategoria: "boxeo",
+  imagen: "30.jpg",
+  precio: 75000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Cuero sintético y relleno de tela"
+},
+// Objeto 31
+{
+  idProducto: "E78K9mnbG654",
+  nombreProducto: "Gafas de natación",
+  selectorCategoria: "natacion",
+  imagen: "31.jpg",
+  precio: 25000,
+  color: "Azul y negro",
+  garantia: "6 meses",
+  material: "Silicona y policarbonato"
+},
+// Objeto 32
+{
+  idProducto: "F78K9mnbG654",
+  nombreProducto: "Pantalones cortos de running",
+  selectorCategoria: "running",
+  imagen: "32.jpg",
+  precio: 35000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Poliéster transpirable"
+},
+// Objeto 33
+{
+  idProducto: "G78K9mnbG654",
+  nombreProducto: "Bolsa para bicicleta",
+  selectorCategoria: "ciclismo",
+  imagen: "33.jpg",
+  precio: 40000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Nylon resistente"
+},
+// Objeto 34
+{
+  idProducto: "H78K9mnbG654",
+  nombreProducto: "Barra de dominadas",
+  selectorCategoria: "gym",
+  imagen: "34.jpg",
+  precio: 90000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Acero galvanizado"
+},
+// Objeto 35
+{
+  idProducto: "I78K9mnbG654",
+  nombreProducto: "Tabla de equilibrio",
+  selectorCategoria: "gym",
+  imagen: "35.jpg",
+  precio: 55000,
+  color: "Madera natural",
+  garantia: "1 año",
+  material: "Madera de abedul"
+},
+// Objeto 36
+{
+  idProducto: "J78K9mnbG654",
+  nombreProducto: "Banda de resistencia",
+  selectorCategoria: "gym",
+  imagen: "36.jpg",
+  precio: 20000,
+  color: "Azul",
+  garantia: "1 año",
+  material: "Látex de alta resistencia"
+},
+// Objeto 37
+{
+  idProducto: "K78K9mnbG654",
+  nombreProducto: "Cuerda de escalada",
+  selectorCategoria: "escalada",
+  imagen: "37.jpg",
+  precio: 120000,
+  color: "Rojo y blanco",
+  garantia: "2 años",
+  material: "Poliamida y poliéster"
+},
+// Objeto 38
+{
+  idProducto: "L78K9mnbG654",
+  nombreProducto: "Gorra de natación",
+  selectorCategoria: "natacion",
+  imagen: "38.jpg",
+  precio: 15000,
+  color: "Negro",
+  garantia: "6 meses",
+  material: "Silicona resistente al cloro"
+},
+// Objeto 39
+{
+  idProducto: "M78K9mnbG654",
+  nombreProducto: "Set de golf",
+  selectorCategoria: "golf",
+  imagen: "39.jpg",
+  precio: 200000,
+  color: "Negro y plateado",
+  garantia: "1 año",
+  material: "Acero inoxidable y grafito"
+},
+// Objeto 40
+{
+  idProducto: "N78K9mnbG654",
+  nombreProducto: "Pelota de golf",
+  selectorCategoria: "golf",
+  imagen: "40.jpg",
+  precio: 30000,
+  color: "Blanco",
+  garantia: "6 meses",
+  material: "Uretano"
+},
+// Objeto 41
+{
+  idProducto: "O78K9mnbG654",
+  nombreProducto: "Bañador de competición",
+  selectorCategoria: "natacion",
+  imagen: "41.jpg",
+  precio: 80000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Poliéster y elastano"
+},
+// Objeto 42
+{
+  idProducto: "P78K9mnbG654",
+  nombreProducto: "Ruedas para patines",
+  selectorCategoria: "patinaje",
+  imagen: "42.jpg",
+  precio: 50000,
+  color: "Blanco",
+  garantia: "6 meses",
+  material: "Poliuretano"
+},
+// Objeto 43
+{
+  idProducto: "Q78K9mnbG654",
+  nombreProducto: "Raqueta de squash",
+  selectorCategoria: "squash",
+  imagen: "43.jpg",
+  precio: 120000,
+  color: "Negro y amarillo",
+  garantia: "1 año",
+  material: "Grafito y titanio"
+},
+// Objeto 44
+{
+  idProducto: "R78K9mnbG654",
+  nombreProducto: "Mochila de senderismo",
+  selectorCategoria: "senderismo",
+  imagen: "44.jpg",
+  precio: 90000,
+  color: "Gris",
+  garantia: "2 años",
+  material: "Nylon impermeable"
+},
+// Objeto 45
+{
+  idProducto: "S78K9mnbG654",
+  nombreProducto: "Set de esquí",
+  selectorCategoria: "esqui",
+  imagen: "45.jpg",
+  precio: 400000,
+  color: "Rojo y negro",
+  garantia: "2 años",
+  material: "Fibra de carbono y aluminio"
+},
+// Objeto 46
+{
+  idProducto: "T78K9mnbG654",
+  nombreProducto: "Sudadera de entrenamiento",
+  selectorCategoria: "gym",
+  imagen: "46.jpg",
+  precio: 60000,
+  color: "Gris",
+  garantia: "1 año",
+  material: "Algodón y poliéster"
+},
+// Objeto 47
+{
+  idProducto: "U78K9mnbG654",
+  nombreProducto: "Red de béisbol",
+  selectorCategoria: "beisbol",
+  imagen: "47.jpg",
+  precio: 25000,
+  color: "Rojo",
+  garantia: "6 meses",
+  material: "Nylon resistente"
+},
+// Objeto 48
+{
+  idProducto: "V78K9mnbG654",
+  nombreProducto: "Pantalones de yoga",
+  selectorCategoria: "yoga",
+  imagen: "48.jpg",
+  precio: 35000,
+  color: "Negro",
+  garantia: "1 año",
+  material: "Algodón orgánico"
+},
+// Objeto 49
+{
+  idProducto: "W78K9mnbG654",
+  nombreProducto: "Set de snorkel",
+  selectorCategoria: "natacion",
+  imagen: "49.jpg",
+  precio: 70000,
+  color: "Azul y negro",
+  garantia: "1 año",
+  material: "Silicona y PVC"
+},
+// Objeto 50
+{
+  idProducto: "X78K9mnbG654",
+  nombreProducto: "Tablero de dardos",
+  selectorCategoria: "dardos",
+  imagen: "50.jpg",
+  precio: 40000,
+  color: "Verde y negro",
+  garantia: "1 año",
+  material: "Cerdas de sisal y plástico"
+}
+
+
+];
+
+console.log(productos)
